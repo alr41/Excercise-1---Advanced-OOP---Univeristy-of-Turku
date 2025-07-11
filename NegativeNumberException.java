7ea2e47957579ff2629bb37ccef8b539de2e9d28
alr41 <75192268+alr41@users.noreply.github.com>
2025-07-11 20:36:16 -0300

    Update NegativeNumberException.java

--- NegativeNumberException.java
+++ NegativeNumberException.java
@@ -1,12 +1,16 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
 class NegativeNumberException extends Exception {
   private final List<Integer> negativeIndices; // Stores the negative numbers' indices
 
   public NegativeNumberException(List<Integer> negativeIndices) {
     super("Negative numbers were found at indices: " + negativeIndices);
-    this.negativeIndices = negativeIndices;
+    this.negativeIndices = new ArrayList<>(negativeIndices);
   }
 
   public List<Integer> getNegativeIndices() {
-    return negativeIndices;
+    return Collections.unmodifiableList(negativeIndices);
   }
 }
