513653ca51a8c661a129f949974b560b583da42d
alr41 <75192268+alr41@users.noreply.github.com>
2025-07-11 20:32:22 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,26 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class Main {
   public static void main(String[] args) {
-    //
+    int[] nums = new int[] {-1, 1, 2};
+    Float result;
+    
+    try {
+      result = avg(nums);
+      System.out.println(result);
+    } catch (EmptyArray e) {
+      System.out.println("Array is empty.");
+    } catch (NegativeNumberException e) {
+      for (int index : e.getNegativeIndices()) {
+        int value = nums[index];
+        int position = index + 1;
+        System.out.println("The " + position + ordinal(position) +
+          " number " + value + " in your array is invalid");
+      }
+    }
   }
-
-  public static float avg(int[] nums) throws EmptyArray, NegativeNumberException {
+    public static float avg(int[] nums) throws EmptyArray, NegativeNumberException {
     int sum = 0;
     if (nums == null || nums.length == 0)
       throw new EmptyArray();
@@ -23,3 +40,19 @@ public class Main {
     }
     return (float) sum / nums.length;
   }
+    public static String ordinal(int n) {
+    if (n % 100 >= 11 && n % 100 <= 13) {
+        return "th";
+    } else if (n % 10 == 1) {
+        return "st";
+    } else if (n % 10 == 2) {
+        return "nd";
+    } else if (n % 10 == 3) {
+        return "rd";
+    } else {
+        return "th";
+    }
+  }
+}
+  
+ 
