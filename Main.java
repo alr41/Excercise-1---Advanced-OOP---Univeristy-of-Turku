1d3ca0c3e666009a6176a6c3a7ce97a01a4840cc
alr41 <75192268+alr41@users.noreply.github.com>
2025-07-10 21:47:52 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,24 @@
+public class Main {
+  public static void main(String[] args) {
+    //
+  }
 
+  public static float avg(int[] nums) throws EmptyArray, NegativeNumbersException {
+    int sum = 0;
+    if (nums == null || nums.length == 0)
+      throw new EmptyArray();
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] < 0) {
+        negativeIndices.add(i);
+      }
+    }
+
+    if (!negativeIndices.isEmpty()) {
+      throw new NegativeNumbersException(negativeIndices);
+    }
+
+    for(int n: nums) {
+      sum += n;
+    }
+    return (float) sum / nums.length;
+  }
